45d57683b1250b53852a869d2cd78e210a7a0055
Chitosastiki <110228508+Chitosastiki@users.noreply.github.com>
2022-07-29 16:00:37 +0400

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,9 +2,9 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
+        int variable  = 10;
         int variable_one = 1;
-        int variable_two = 2;
+        int variableTwo = 2;
         int variable_three = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE= 5;
